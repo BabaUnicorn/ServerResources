eacac75dc962d34922a2e19ace31d5d2f050048a
Louis <77413577+lvlou@users.noreply.github.com>
2021-03-14 18:23:49 +0000

    first try at loading cayo perico island

---
+++ cayo/client.js
@@ -0,0 +1,18 @@
+let wait = (ms) => new Promise(res => setTimeout(res, ms));
+
+let islandCoords = [4840.571, -5174.425, 2.0]
+
+async function loadCayo(){
+    while(true){
+        let playerCoords = GetEntityCoords(PlayerPedId())
+        let distance = playerCoords - islandCoords
+        if(distance < 2000){
+            Citizen.InvokeNative("0x9A9D1BA639675CF1", "HeistIsland", true)  // load the map and removes the city
+			Citizen.InvokeNative("0x5E1460624D194A38", true) // load the minimap/pause map and removes the city minimap/pause map
+        } else {
+            Citizen.InvokeNative("0x9A9D1BA639675CF1", "HeistIsland", false)  // load the map and removes the city
+			Citizen.InvokeNative("0x5E1460624D194A38", false) // load the minimap/pause map and removes the city minimap/pause map
+        }
+        await wait(5000)
+    }
+}
