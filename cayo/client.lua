d6e08bb32880a5270872db1dd8f66b5f10eaf100
Freddy <33939455+freedy69@users.noreply.github.com>
2021-03-14 23:25:30 +0400

    Update client.lua

--- cayo/client.lua
+++ cayo/client.lua
@@ -4,14 +4,14 @@ Citizen.InvokeNative(0x53797676AD34A9AA, false)
 SetScenarioGroupEnabled('Heist_Island_Peds', true)
 
 -- audio stuff
-SetAudioFlag('PlayerOnDLCHeist4Island', true)
 SetAmbientZoneListStatePersistent('AZL_DLC_Hei4_Island_Zones', true, true)
 SetAmbientZoneListStatePersistent('AZL_DLC_Hei4_Island_Disabled_Zones', false, true)
 
 function ToggleCayo (toggle)
   Citizen.InvokeNative(0x9A9D1BA639675CF1, 'HeistIsland', toggle) -- or use false to disable it
   -- instead of using island hopper you can *also* just load the IPLs mentioned in islandhopper.meta yourself somewhat
-
+  SetAudioFlag('PlayerOnDLCHeist4Island', toggle)
+  
   -- switch radar interior
   Citizen.InvokeNative(0x5E1460624D194A38, toggle)
 end
