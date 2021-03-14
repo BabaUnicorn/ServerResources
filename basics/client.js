399046a8fac24c6722f904d5ad5121652458c6f1
Louis <77413577+lvlou@users.noreply.github.com>
2021-03-14 21:16:25 +0000

    Update client.js

--- basics/client.js
+++ basics/client.js
@@ -28,8 +28,8 @@ let WAIT = (ms) => new Promise(res => setTimeout(res, ms));
 let ped = PlayerPedId();
 
 async function dmNotifRec(player, message){
-    let txdDict = 'frontend'
-    let txtName = 'gtav_logo'
+    let txdDict = 'mpawardheists'
+    let txtName = 'thepostman'
     RequestStreamedTextureDict(txdDict)
     while(!HasStreamedTextureDictLoaded){
         await WAIT(0)
@@ -45,8 +45,8 @@ async function dmNotifRec(player, message){
 }
 
 async function dmNotifSent(player, message){
-    let txdDict = 'frontend'
-    let txtName = 'gtav_logo'
+    let txdDict = 'mpawardheists'
+    let txtName = 'thepostman'
     RequestStreamedTextureDict(txdDict)
     while(!HasStreamedTextureDictLoaded){
         await WAIT(0)
