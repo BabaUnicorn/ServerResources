c3efb56c749d0c9a34905df7e35112e24d036cd1
Louis <77413577+lvlou@users.noreply.github.com>
2021-03-14 19:40:37 +0000

    Update client.js

--- basics/client.js
+++ basics/client.js
@@ -47,7 +47,6 @@ async function dmNotifRec(player, message, headshot){
 async function dmNotifSent(player, message, headshot){
     let handle = RegisterPedheadshotTransparent(PlayerPedId())
     while(!IsPedheadshotReady(handle) || !IsPedheadshotValid(handle)){
-        console.log('cant get headshot')
         await WAIT(0)
     }
     let txd = GetPedheadshotTxdString(handle)
