ea34cecc7091fb234e685ce5d23b04903674f205
Freddy <33939455+freedy69@users.noreply.github.com>
2021-03-14 23:22:17 +0400

    Update client.lua

--- cayo/client.lua
+++ cayo/client.lua
@@ -1,24 +1,31 @@
-function ToggleIsland(enabled)
-  Citizen.InvokeNative(0x9A9D1BA639675CF1, "HeistIsland", enabled) -- Toggle island gta5 level
-  Citizen.InvokeNative(0x5E1460624D194A38, enabled) -- Toggle island minimap
+-- misc natives
+Citizen.InvokeNative(0xF74B1FFA4A15FBEA, true)
+Citizen.InvokeNative(0x53797676AD34A9AA, false)    
+SetScenarioGroupEnabled('Heist_Island_Peds', true)
+
+-- audio stuff
+SetAudioFlag('PlayerOnDLCHeist4Island', true)
+SetAmbientZoneListStatePersistent('AZL_DLC_Hei4_Island_Zones', true, true)
+SetAmbientZoneListStatePersistent('AZL_DLC_Hei4_Island_Disabled_Zones', false, true)
+
+function ToggleCayo (toggle)
+  Citizen.InvokeNative(0x9A9D1BA639675CF1, 'HeistIsland', toggle) -- or use false to disable it
+  -- instead of using island hopper you can *also* just load the IPLs mentioned in islandhopper.meta yourself somewhat
+
+  -- switch radar interior
+  Citizen.InvokeNative(0x5E1460624D194A38, toggle)
 end
 
-function ToggleIslandPathNodes(enabled)
-  Citizen.InvokeNative(0xF74B1FFA4A15FBEA, enabled) -- Toggle island path nodes
-end
-
-local IslandIsEnabled = false
-
-RegisterCommand('cayo', function()
-        if IslandIsEnabled then
-            IslandIsEnabled = true
-            ToggleIsland(IslandIsEnabled)
-            ToggleIslandPathNodes(IslandIsEnabled)
-        else
-            IslandIsEnabled = false
-            ToggleIsland(IslandIsEnabled)
-            ToggleIslandPathNodes(IslandIsEnabled)            
-        end
+local IslandisEnabled = false
+RegisterCommand('cayo', function ()
+    if IslandIsEnabled then
+      IslandIsEnabled = false
+      ToggleCayo(IslandIsEnabled)
+      print('disabled prostituta')
+    else
+      IslandIsEnabled = true
+      ToggleCayo(IslandIsEnabled)
+      print('enabled prostituta')
+    end
+      
 end)
-
-print('bababitch')
