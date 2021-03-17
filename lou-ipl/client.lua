25e3e04c80771a476419e321df6c0a12b0fece25
Louis <77413577+lvlou@users.noreply.github.com>
2021-03-17 18:01:27 +0000

    ipl loader v1

---
+++ lou-ipl/client.lua
@@ -0,0 +1,147 @@
+local carrier = {
+    'hei_carrier',
+    'hei_carrier_DistantLights',
+    'hei_Carrier_int1',
+    'hei_Carrier_int2',
+    'hei_Carrier_int3',
+    'hei_Carrier_int4',
+    'hei_Carrier_int5',
+    'hei_Carrier_int6',
+    'hei_carrier_LODLights'
+}
+
+RequestIpl(carrier[0])
+RequestIpl(carrier[1])
+RequestIpl(carrier[2])
+RequestIpl(carrier[3])
+RequestIpl(carrier[4])
+RequestIpl(carrier[5])
+RequestIpl(carrier[6])
+RequestIpl(carrier[7])
+RequestIpl(carrier[8])
+
+local yacht = {
+    'smboat',
+    'smboat_lod',
+    'hei_yacht_heist',
+    'hei_yacht_heist_Bar',
+    'hei_yacht_heist_Bedrm',
+    'hei_yacht_heist_Bridge',
+    'hei_yacht_heist_DistantLights',
+    'hei_yacht_heist_enginrm',
+    'hei_yacht_heist_LODLights',
+    'hei_yacht_heist_Lounge'	
+}
+
+RequestIpl(yacht[0])
+RequestIpl(yacht[1])
+RequestIpl(yacht[2])
+RequestIpl(yacht[3])
+RequestIpl(yacht[4])
+RequestIpl(yacht[5])
+RequestIpl(yacht[6])
+RequestIpl(yacht[7])
+RequestIpl(yacht[8])
+RequestIpl(yacht[9])
+
+local simeons = {
+    'shr_int',
+    'shr_int_lod'
+}
+
+RequestIpl(simeons[0])
+RequestIpl(simeons[1])
+
+local ferriswheel = {
+    'ferris_finale_anim',
+    'ferris_finale_anim_lod'
+}
+
+RequestIpl(ferriswheel[0])
+RequestIpl(ferriswheel[1])
+
+local lifeinvader = {
+    'facelobby',
+    'facelobby_lod'
+}
+
+RequestIpl(lifeinvader[0])
+RequestIpl(lifeinvader[1])
+
+local casino = {
+    'vw_casino_main',
+    'vw_casino_garage',
+    'vw_casino_carpark',
+    'vw_casino_penthouse'
+}
+
+RequestIpl(casino[0])
+RequestIpl(casino[1])
+RequestIpl(casino[2])
+RequestIpl(casino[3])
+
+local northyankton = {
+    'prologue01',
+    'prologue01c',
+    'prologue01d',
+    'prologue01e',
+    'prologue01f',
+    'prologue01g',
+    'prologue01h',
+    'prologue01i',
+    'prologue01j',
+    'prologue01k',
+    'prologue01z',
+    'prologue02',
+    'prologue03',
+    'prologue03b',
+    'prologue03_grv_dug',
+    'prologue_grv_torch',
+    'prologue04',
+    'prologue04b',
+    'prologue04_cover',
+    'des_protree_end',
+    'des_protree_start',
+    'prologue05',
+    'prologue05b',
+    'prologue06',
+    'prologue06b',
+    'prologue06_int',
+    'prologue06_pannel',
+    'plg_occl_00',
+    'prologue_occl',
+    'prologuerd',
+    'prologuerdb'
+}
+
+RequestIpl(northyankton[0])
+RequestIpl(northyankton[1])
+RequestIpl(northyankton[2])
+RequestIpl(northyankton[3])
+RequestIpl(northyankton[4])
+RequestIpl(northyankton[5])
+RequestIpl(northyankton[6])
+RequestIpl(northyankton[7])
+RequestIpl(northyankton[8])
+RequestIpl(northyankton[9])
+RequestIpl(northyankton[10])
+RequestIpl(northyankton[11])
+RequestIpl(northyankton[12])
+RequestIpl(northyankton[13])
+RequestIpl(northyankton[14])
+RequestIpl(northyankton[15])
+RequestIpl(northyankton[16])
+RequestIpl(northyankton[17])
+RequestIpl(northyankton[18])
+RequestIpl(northyankton[19])
+RequestIpl(northyankton[20])
+RequestIpl(northyankton[21])
+RequestIpl(northyankton[22])
+RequestIpl(northyankton[23])
+RequestIpl(northyankton[24])
+RequestIpl(northyankton[25])
+RequestIpl(northyankton[26])
+RequestIpl(northyankton[27])
+RequestIpl(northyankton[28])
+RequestIpl(northyankton[29])
+RequestIpl(northyankton[30])
