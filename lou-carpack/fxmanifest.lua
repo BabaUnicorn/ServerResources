12faddd27d4c05e6cae11e88cb06632dd4e4ad80
Louis <77413577+lvlou@users.noreply.github.com>
2021-03-22 14:23:45 +0000

    Add files via upload

---
+++ lou-carpack/fxmanifest.lua
@@ -0,0 +1,34 @@
+-- car pack
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+    '/meta/s13/carcols.meta',
+    '/meta/s13/carvariations.meta',
+    '/meta/s13/dlctext.meta',
+    '/meta/s13/handling.meta',
+    '/meta/s13/nis13contentunlocks.meta',
+    '/meta/s13/vehicles.meta',
+    '/meta/s14/carcols.meta',
+    '/meta/s14/carvariations.meta',
+    '/meta/s14/dlctext.meta',
+    '/meta/s14/handling.meta',
+    '/meta/s14/vehicles.meta',
+    '/meta/wheelpack/dlctext.meta',
+    '/meta/wheelpack/carcols.meta',
+    '/meta/wheelpack/hotboiwheelscontentunlocks.meta'
+}
+
+data_file 'HANDLING_FILE' '/meta/s13/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '/meta/s13/vehicles.meta'
+data_file 'CARCOLS_FILE' '/meta/s13/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '/meta/s13/carvariations.meta'
+
+data_file 'HANDLING_FILE' '/meta/s14/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '/meta/s14/vehicles.meta'
+data_file 'CARCOLS_FILE' '/meta/s14/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '/meta/s14/carvariations.meta'
+
+data_file 'CARCOLS_FILE' '/meta/wheelpack/carcols.meta'
+
+-- data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
